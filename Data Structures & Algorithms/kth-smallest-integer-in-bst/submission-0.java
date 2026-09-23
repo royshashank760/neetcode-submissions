0916796fccdc6498ac2c52fc1b94ab876e213275
Shashank Rai <royshashank760@gmail.com>
2026-09-23 15:08:30 +0530

    Add: kth-smallest-integer-in-bst - submission-0

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.java
@@ -0,0 +1,27 @@
+
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (!stack.isEmpty() || root != null) {
+
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            k--;
+
+            if (k == 0) {
+                return root.val;
+            }
+
+            root = root.right;
+        }
+
+        return -1;
+    }
+}
